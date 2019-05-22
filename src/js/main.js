d6727b20c504bf0912dda277c52e664a67aa6d89
Angelino Pires <lino.pires@live.com>
2019-05-22 00:11:00 -0300

    modal criada

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,25 @@
 const submit = document.querySelector('.form-control .form__button');
+const requests = Array.from(document.querySelectorAll('.request-list .request'));
+const modal = document.querySelector('.modal');
+
+console.log(requests);
+
 
 if (submit) {
   submit.addEventListener('click', () => window.location.href = "/solicitar.html");
+}
+
+if (requests) {
+  requests.map(request => {
+    request.addEventListener('click', () => {
+
+      if (modal) {
+        modal.classList.toggle('show');
+      }
+    });
+  })
+}
+
+if (modal) {
+  modal.addEventListener('click', () => modal.classList.toggle('show'));
 }
